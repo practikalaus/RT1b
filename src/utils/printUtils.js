82ed0e4bc55ec5fafa25a21ebea8995722ce88b2
practikalaus <147788708+practikalaus@users.noreply.github.com>
2025-07-12 15:10:58 +0800

    Create printUtils.js

---
+++ src/utils/printUtils.js
@@ -0,0 +1,14 @@
+export const printHTMLContent = (html, title = 'document', styles = '') => {
+  const printWindow = window.open('', '_blank');
+  if (!printWindow) {
+    alert('Unable to open print window. Please allow pop-ups for this site.');
+    return;
+  }
+
+  printWindow.document.open();
+  printWindow.document.write(`<!DOCTYPE html><html><head><title>${title}</title><style>@media print{body{-webkit-print-color-adjust:exact;}}</style><style>${styles}</style></head><body>${html}</body></html>`);
+  printWindow.document.close();
+
+  printWindow.focus();
+  printWindow.print();
+};
